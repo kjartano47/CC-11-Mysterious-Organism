9874b63c27f8e8c8a5774eb4c3068dbc3513d5e3
kjartano47 <kjartano97@gmail.com>
2022-11-22 20:57:26 +0000

    Update main.js

--- main.js
+++ main.js
@@ -14,11 +14,18 @@ const mockUpStrand = () => {
 };
 
 const pAequorFactory = (num, arr) => {
-	return {
+	const newSpecimen = {
 		specimenNum: num,
 		dna: arr,
-		mutate() {},
+		mutate() {
+			console.log("Hello1");
+			return (mutatedBase = this.dna[Math.floor(Math.random() * 4)]);
+		},
 	};
+	console.log("Hello2");
+	return newSpecimen;
 };
 
 console.log(pAequorFactory(1, mockUpStrand()));
+console.log(pAequorFactory.dna);
+pAequorFactory(1, mockUpStrand().mutate);
