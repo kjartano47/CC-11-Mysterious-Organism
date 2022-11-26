bff6a98ac70c43c16fd931c1167dc20f1c40514f
kjartano47 <kjartano97@gmail.com>
2022-11-26 14:48:26 +0000

    Completed Step 5

--- main.js
+++ main.js
@@ -14,18 +14,72 @@ const mockUpStrand = () => {
 };
 
 const pAequorFactory = (num, arr) => {
-	const newSpecimen = {
+	return {
 		specimenNum: num,
 		dna: arr,
 		mutate() {
-			console.log("Hello1");
-			return (mutatedBase = this.dna[Math.floor(Math.random() * 4)]);
+			console.log("Specimen has begun mutating...");
+			//picking a random dna base location
+			randIndex = Math.floor(Math.random() * this.dna.length);
+			console.log("mutation on base: " + randIndex);
+			//create a new base
+			let newBase = returnRandBase();
+			while (newBase === this.dna[randIndex]) {
+				newBase = returnRandBase();
+			}
+			//assigning newBase to the random location
+			console.log(
+				"Mutation has occurred at: " +
+					randIndex +
+					" with the base: " +
+					newBase
+			);
+			this.dna[randIndex] = newBase;
+
+			return this.dna;
+		},
+		compareDNA(pAequor) {
+			//pass in an object not an arr
+			let match = 0;
+			for (let i = 0; i < pAequor.dna.length; i++) {
+				if (this.dna[i] === pAequor.dna[i]) {
+					match++;
+					console.log("Match!");
+				}
+			}
+			console.log(
+				" The percentage of DNA specimen: #" +
+					pAequor.specimenNum +
+					" and #" +
+					this.specimenNum +
+					" have in common is " +
+					((match / pAequor.dna.length) * 100).toFixed() +
+					"%"
+			);
 		},
 	};
-	console.log("Hello2");
-	return newSpecimen;
 };
 
-console.log(pAequorFactory(1, mockUpStrand()));
-console.log(pAequorFactory.dna);
-pAequorFactory(1, mockUpStrand().mutate);
+let num1 = 1;
+let testArr = [
+	"C",
+	"G",
+	"T",
+	"T",
+	"C",
+	"G",
+	"T",
+	"T",
+	"T",
+	"T",
+	"A",
+	"A",
+	"T",
+	"T",
+	"G",
+];
+
+let testObject1 = pAequorFactory(2, mockUpStrand());
+
+let outcome = pAequorFactory(num1, testArr);
+console.log(outcome.compareDNA(testObject1));
